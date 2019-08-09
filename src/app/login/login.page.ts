20695033f1918501f8b61a745137df1869cf7888
Felipe Côrtes <contatos.cortes@gmail.com>
2019-08-09 14:28:22 -0300

    + Adicionado dependência de QR-Code; + Adicionado title e autor na config de build; + Removido card do login.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Platform} from "@ionic/angular";
 
 @Component({
     selector: 'app-login',
@@ -7,13 +8,21 @@ import {Router} from '@angular/router';
     styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+    public isMobile: boolean;
 
-    constructor(private route: Router) { }
+    constructor(private route: Router, private plt: Platform) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.isMobile = this.plt.is('ios') || this.plt.is('android');
+/*        console.log('IOS: ' + this.plt.is('ios'));
+        console.log('Android: ' + this.plt.is('android'));
+        console.log('Mobile: ' + this.plt.is('mobile'));
+        console.log('Mobile Web: ' + this.plt.is('mobileweb'));
+        console.log('Cordova: ' + this.plt.is('cordova'));*/
+    }
 
     public login() {
-        this.route.navigate(['']);
+        this.route.navigate(['']).catch(err => console.log(err));
     }
 
 }
